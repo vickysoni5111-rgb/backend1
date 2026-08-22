b2e5abda7305a7e52532e938e6e51dd7cd20e2d7
vickysoni5111-rgb <vickysoni5111@gmail.com>
2026-08-22 12:00:20 +0530

    speed up email sending

--- emailService.js
+++ emailService.js
@@ -1,11 +1,17 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD,
   },
+  pool: true,
+  maxConnections: 3,
+  maxMessages: 50,
+  connectionTimeout: 10000,
 });
 
 const escapeHtml = (value = "") => {
